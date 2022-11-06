8d5f808698315a6da414aaeb075f5e1c0de00da5
Lord1k <109299534+Lord1k-x@users.noreply.github.com>
2022-11-06 23:30:48 +0300

    Create Rejoiner.lua

---
+++ Rejoiner.lua
@@ -0,0 +1,2 @@
+--By Lord1k#2111--
+game:GetService('Players').LocalPlayer:Kick("Rejoin method by Lord1k") game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
